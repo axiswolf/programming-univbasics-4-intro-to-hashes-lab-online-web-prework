2fbecba5ea2d0ed2ba64cf0523e02e4eb7e618df
Karen V <karenvo@usc.edu>
2019-12-26 12:19:40 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,12 +13,12 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  {name: => "Grace Hopper"}
+  {:name => "Grace Hopper"}
 end
 
 def id_generator
   # return a hash with a key :id assigned to positive integer
-  {id: => 2}
+  {:id => 2}
 end
 
 def my_hash_creator(key, value)
