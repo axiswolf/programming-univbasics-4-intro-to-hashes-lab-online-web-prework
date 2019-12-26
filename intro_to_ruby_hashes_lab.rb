219d1fc0cdcc47e9dfdc8c6fa98bc6e71bb4befa
Karen V <karenvo@usc.edu>
2019-12-26 12:12:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -6,13 +6,16 @@ end
 def my_hash
   # return a valid hash with any key/value pair of your choice
   person = {
-  name: "Sam",
-  age: 31
+  name: "Bleh",
+  age: 23
   }
 end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: "Grace Hopper"
+  }
 end
 
 def id_generator
